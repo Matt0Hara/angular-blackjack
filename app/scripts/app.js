6f412da439609ed7ee38dcaba08534f4997ae9f8
Matt0Hara <mohara2@worcester.edu>
2015-11-17 17:04:28 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name blackjackApp
+ * @description
+ * # blackjackApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('blackjackApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blackjackApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blackjackApp
+ */
+angular.module('blackjackApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
